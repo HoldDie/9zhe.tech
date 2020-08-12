87d5b6b1627e23c05630627f9d774cfaa23909e1
zeyangg <zeyangg@cn.ibm.com>
2020-08-12 22:50:55 +0800

    fix: modify gittalk token

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -173,11 +173,11 @@ module.exports = {
             {
                 choosen: 'gitalk',
                 options: {
-                    clientID: 'a6e1355287947096b88b',
-                    clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-                    repo: 'vuepress-theme-vdoing', // GitHub 仓库
-                    owner: 'xugaoyi', // GitHub仓库所有者
-                    admin: ['xugaoyi'], // 对仓库有写权限的人
+                    clientID: 'a0bbecf67d8ee3a18a07',
+                    clientSecret: '000d754614933ed55fdafc50c43664975533a66b',
+                    repo: '9zhe.tech', // GitHub 仓库
+                    owner: 'holddie', // GitHub仓库所有者
+                    admin: ['holddie'], // 对仓库有写权限的人
                     // distractionFreeMode: true,
                     pagerDirection: 'last', // 'first'正序 | 'last'倒序
                     id: "<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>", //  页面的唯一标识,长度不能超过50
