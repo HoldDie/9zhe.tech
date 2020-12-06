15f2306606dedc96e84f30a2d15a9d76ffd228d6
holddie <holddie@163.com>
2020-12-07 00:17:55 +0800

    fix: remove auto GitTalk

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,7 @@ git push -f $githubUrl master:gh-pages # 推送到github
 
 echo "进行附加操作"
 cd ../../../
-npm run autoGitTalk ${GITHUB_TOKEN}
+# npm run autoGitTalk ${GITHUB_TOKEN}
 # deploy to coding
 #echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
 #if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
